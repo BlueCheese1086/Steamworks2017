ad08841c297fd04ff37a0721c929253eceb9585c
hcps-mcdoweljn <hcps-mcdoweljn@180-18MCDOWELJN.hcps.internal>
2017-01-14 09:31:00 -0500

    Basic Drive Code

--- src/org/usfirst/frc/team1086/robot/Robot.java
+++ src/org/usfirst/frc/team1086/robot/Robot.java
@@ -1,27 +1,22 @@
 package org.usfirst.frc.team1086.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser<String> chooser = new SendableChooser<>();
-
-    @Override
-    public void robotInit(){}
-
-    @Override
-    public void autonomousInit(){}
-
-    @Override
-    public void autonomousPeriodic(){}
-
-    @Override
-    public void teleopPeriodic(){}
-
-    @Override
-    public void testPeriodic(){}
+    Drivetrain drive;
+    Joystick leftStick;
+    Joystick rightStick;
+    @Override public void robotInit(){
+        drive = new Drivetrain();
+        leftStick = new Joystick(RobotMap.LEFT_STICK);
+        rightStick = new Joystick(RobotMap.RIGHT_STICK);
+    }
+    @Override public void autonomousInit(){}
+    @Override public void autonomousPeriodic(){}
+    @Override public void teleopPeriodic(){
+        drive.drive(leftStick.getY(), leftStick.getX(), rightStick.getX(), rightStick.getRawButton(1));
+    }
+    @Override public void testPeriodic(){}
 }
 
