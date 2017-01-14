c36ef3caff3365e52509f0120f8477f8bd6aa797
hcps-mcdoweljn <hcps-mcdoweljn@180-18MCDOWELJN.hcps.internal>
2017-01-14 09:28:15 -0500

    Created RobotMap

---
+++ src/org/usfirst/frc/team1086/robot/RobotMap.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team1086.robot;
+public class RobotMap {
+    public static final int LEFT_FRONT_MECANUM = 1;
+    public static final int LEFT_FRONT_COLSON = 2;
+    public static final int LEFT_REAR_MECANUM = 3;
+    public static final int LEFT_REAR_COLSON = 4;
+    public static final int RIGHT_FRONT_MECANUM = 5;
+    public static final int RIGHT_FRONT_COLSON = 6;
+    public static final int RIGHT_REAR_MECANUM = 7;
+    public static final int RIGHT_REAR_COLSON = 8;
+    
+    public static final int LEFT_TRIGGER = 1;
+    public static final int RIGHT_TRIGGER = 2;
+}
