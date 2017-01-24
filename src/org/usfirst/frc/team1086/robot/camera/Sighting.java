9d69d2594909898882756888e7412825ffb54986
hcps-dikejr <hcps-dikejr@180-20DIKEJR.hcps.internal>
2017-01-24 18:05:59 -0500

    I did stuff Jack told me to do.

--- src/org/usfirst/frc/team1086/robot/camera/Sighting.java
+++ src/org/usfirst/frc/team1086/robot/camera/Sighting.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1086.robot.camera;
 
 import org.opencv.core.MatOfPoint;
+import org.opencv.imgproc.Imgproc;
 
 public class Sighting {
     int x, y;
@@ -16,6 +17,14 @@ public class Sighting {
         return false;
     }
     public Sighting(MatOfPoint m){
-        
+        height=m.height();
+        width=m.width();
+        x = Imgproc.boundingRect(m).x;
+        y = Imgproc.boundingRect(m).y;
+        area = Imgproc.contourArea(m);
+        solidity = area / (width * height);
+        aspectRatio= width / height;
+        centerX = x+width/2;
+        centerY = y+height/2;
     }
 }
