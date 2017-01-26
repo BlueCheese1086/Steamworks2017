12832a395c379223070d7af7a681768cb1a34378
hcps-ngokk <hcps-ngokk@180-18NGOKK.hcps.internal>
2017-01-26 16:57:01 -0500

    Added more ButtonMap options and fixed climber not stopping

--- src/org/usfirst/frc/team1086/robot/ButtonMap.java
+++ src/org/usfirst/frc/team1086/robot/ButtonMap.java
@@ -1,6 +1,14 @@
 
 package org.usfirst.frc.team1086.robot;
 public class ButtonMap {
+    //LEFT JOYSTICK
+    public static final int SAFETY_DRIVE = 1;
+    
+    //RIGHT JOYSTICK
+    public static final int OCTO_SHIFTER = 1;
+    public static final int GYRO_DRIVE = 2;
+    
+    //AUX JOYSTICK
     public static final int SHOOT = 1;
     public static final int COLLECT = 2;
     public static final int CLIMB = 3;
