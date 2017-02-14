caa9a4a3c7c0200776a0565f980f297c496c99a6
Jack McDowell <kevin.ngo0407@gmail.com>
2017-02-14 17:06:00 -0500

    Fixed drive misunderstanding

--- src/org/usfirst/frc/team1086/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1086/robot/subsystems/Drivetrain.java
@@ -42,15 +42,23 @@ public class Drivetrain implements PIDSource, PIDOutput {
     }
     public void mecanum(double leftY, double leftX, double rightX){
         leftFrontMecanum.set(leftY - rightX - leftX);
+        leftFrontColson.set(leftY - rightX - leftX);
         rightFrontMecanum.set(leftY + rightX + leftX);
+        rightFrontColson.set(leftY + rightX + leftX);
         leftRearMecanum.set(leftY - rightX + leftX);
+        leftRearColson.set(leftY - rightX + leftX);
         rightRearMecanum.set(leftY + rightX - leftX);
+        rightRearColson.set(leftY + rightX - leftX);
     }
     public void colson(double leftY, double rightX){
         leftFrontMecanum.set(leftY - rightX);
+        leftFrontColson.set(leftY - rightX);
         rightFrontMecanum.set(leftY + rightX);
+        rightFrontColson.set(leftY + rightX);
         leftRearMecanum.set(leftY - rightX);
+        leftRearColson.set(leftY - rightX);
         rightRearMecanum.set(leftY + rightX);
+        rightRearColson.set(leftY + rightX);
     }
     public void gyroDrive(double leftY, double leftX, boolean trigger){
         this.trigger.set(trigger);
